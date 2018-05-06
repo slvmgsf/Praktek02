64bf3962c41b26ce3e181838a540010ae67cc462
slvmgsf <34591196+slvmgsf@users.noreply.github.com>
2018-05-06 15:32:57 +0800

    Initial commit

---
+++ Rectangle.java
@@ -0,0 +1,27 @@
+package praktek02;
+public class Rectangle {
+    double panjang;
+    double lebar;
+
+    public Rectangle() {
+        panjang=5;
+        lebar=2;
+    }
+    
+    void cetakInfo(){
+        System.out.println("=====================");
+        System.out.println("Panjang : "+panjang);
+        System.out.println("Lebar :   "+lebar);
+        System.out.println("=====================");
+    }
+    
+    double hitungLuas(){
+        double luas;
+        luas=panjang*lebar;
+        return luas;
+    }
+    
+    void cetakLuas(){
+        System.out.println("Luasnya adalah: "+hitungLuas());
+    }
+}
